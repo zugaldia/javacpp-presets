f8c954eda74e0ecc67ae36b7477c7deaee04d309
Antonio Zugaldia <antonio@mapbox.com>
2020-02-02 14:23:24 -0500

    added pom.xml including module-info.java

---
+++ liboqs/src/main/java9/module-info.java
@@ -0,0 +1,6 @@
+module org.bytedeco.liboqs {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.liboqs;
+  exports org.bytedeco.liboqs.global;
+  exports org.bytedeco.liboqs.presets;
+}
